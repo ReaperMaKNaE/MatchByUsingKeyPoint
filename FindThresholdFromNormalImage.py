18c772f9f44035243dcfe653b051872b7bfbad99
ReaperMaKNaE <57165459+ReaperMaKNaE@users.noreply.github.com>
2020-02-17 13:48:37 +0900

    2020.02.17.sub

--- FindThresholdFromNormalImage.py
+++ FindThresholdFromNormalImage.py
@@ -3,25 +3,19 @@ import FastFeatureDetectForPixel as FFDFP
 import FastFeatureDetector as FFD
 
 
-'''
-inputImage1 = 'normal1.png'
-CutinputImage1 = SDOF.ShapeDetectForSingleImage(inputImage1)
-normalPixel = FFDFP.GetPixelLocationForNormal(CutinputImage1)
 
-print(normalPixel)
+#inputImage1 = 'normal1.jpg'
+#CutinputImage1 = SDOF.ShapeDetectForSingleImage(inputImage1)
 
-
-inputImage2 = 'normal2.jpg'
+inputImage2 = 'defect4.jpg'
 SDOF.ShapeDetectForSingleImage(inputImage2)
 
-inputImage3 = 'defect1.png'
-SDOF.ShapeDetectForSingleImage(inputImage3)
-
-inputImage3 = 'defect2.jpg'
-SDOF.ShapeDetectForSingleImage(inputImage3)
 '''
+inputImage3 = 'normal7.jpg'
+SDOF.ShapeDetectForSingleImage(inputImage3)
 
-
+inputImage3 = 'normal8.jpg'
+SDOF.ShapeDetectForSingleImage(inputImage3)
 
 firstImage = './img/Cutnormal1.png'
 secondImage = './img/Cutnormal2.jpg'
@@ -29,3 +23,4 @@ secondImage = './img/Cutnormal2.jpg'
 thresholdForNormal, thresholdForDefect = FFD.detectDefectUsingThreshold(firstImage, secondImage)
 
 print('threshold Values : ', thresholdForNormal, ' and ', thresholdForDefect)
+'''
